dd00a763ee62df07d166cb4679ec9f89593840bc
Mohd Zia <humobacillus@gmail.com>
2021-08-02 02:30:21 +0530

    Initial Commit

---
+++ BestBusSpringboot/src/main/java/com/example/bestbus/BestBusSpringbootApplication.java
@@ -0,0 +1,13 @@
+package com.example.bestbus;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BestBusSpringbootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BestBusSpringbootApplication.class, args);
+	}
+
+}
